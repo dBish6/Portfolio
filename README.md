7fc73163761fb88bdbdb1b3e580d032fdb6f5101
dBish6 <davidbish2002@hotmail.com>
2023-01-07 00:12:12 -0330

    Loader before dom renders.

--- README.md
+++ README.md
@@ -1,11 +1,12 @@
 # Portfolio
-This is my portfolio, a website that's all about me! I am still a new devloper, but this portfolio is better then any new developer's:) I worked very hard on it, if you take a look at it, I hope you like it and get inspired!
 
-__portfolio_v1__<br />
-The first version of my portfolio is pretty basic. I made it early in my coding journey so of course it would be, but it is still good for a start. 
+This is my portfolio, a website that's all about me! I am still a new developer, but this portfolio is better then any new developer's:) I worked very hard on it, if you take a look at it, I hope you like it and get inspired!
 
-__portfolio_v2__<br />
+**portfolio_v1**<br />
+The first version of my portfolio is pretty basic. I made it early in my coding journey so of course it would be, but it is still good for a start.
+
+**portfolio_v2**<br />
 This is the second version of my portfolio, which is the one you would want to look at. This also shows how much I have improved compared to the first version. This version improves everything by a long shot and uses a custom 12 column grid layout in css that I have been learning, first time I used it here.
 
-__Deployed Stable Version of the Website__<br />
-`https://dbish6.github.io/home`
+**Deployed Stable Version of the Website**<br />
+`https://dbish6.github.io/Portfolio/`
--- portfolio_v2/src/App.jsx
+++ portfolio_v2/src/App.jsx
@@ -4,7 +4,7 @@
    Creation Date: December 24, 2022
 */
 
-import React from "react";
+import { useState, useEffect } from "react";
 import {
   BrowserRouter,
   Routes,
@@ -24,20 +24,35 @@ import Footer from "./components/partials/Footer";
 import Home from "./pages/Home";
 import Loading from "./pages/Loading";
 
-import ToDoProject from "./featuredProjects/ToDoProject";
+import ToDoProject from "./featuredProjects/TodoApp";
+import CalculatorProject from "./featuredProjects/calculatorApp";
 
 import Error404 from "./pages/errors/Error404";
 
 function App() {
+  const [DomLoading, setDomLoading] = useState(true);
+
+  useEffect(() => {
+    const loaderBG = document.querySelector(".loaderBG");
+    const loaderContainer = document.querySelector(".loaderContainer");
+    if (loaderBG || loaderContainer) {
+      setTimeout(() => {
+        if (DomLoading) {
+          loaderBG.remove();
+          loaderContainer.remove();
+          setDomLoading(false);
+        }
+      }, 1000);
+    }
+  });
+
   const ShowPartials = () => (
     <>
       <Navigation />
       {/* Nested routes render out here. */}
-      {/* <div className="center"> */}
       <main className="gridContainer">
         <Outlet />
       </main>
-      {/* </div> */}
       <Footer />
     </>
   );
@@ -46,20 +61,29 @@ function App() {
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/loading" element={<Loading title="Loading" />} />
+          <Route
+            path="/Portfolio/loading"
+            element={<Loading title="Loading" />}
+          />
           <Route element={<ShowPartials />}>
-            <Route path="/" element={<Home title="Home" />} />
-            <Route path="/home" element={<Home title="Home" />} />
+            <Route path="/Portfolio/" element={<Home title="Home" />} />
+            <Route path="/Portfolio/home" element={<Home title="Home" />} />
 
-            <Route path="/error404" element={<Error404 title="ERROR" />} />
+            <Route
+              path="/Portfolio/error404"
+              element={<Error404 title="ERROR" />}
+            />
             <Route path="*" element={<Error404 title="ERROR" />} />
             {/* <Route path="*" render={() => <Navigate to="/error404" />} /> */}
           </Route>
           <Route
-            path="/projects/todo"
+            path="/Portfolio/projects/todo"
             element={<ToDoProject title="Todo App" />}
           />
-          {/* <Route path="/projects/calculator" element={<ToDoProject />} /> */}
+          <Route
+            path="/Portfolio/projects/calculator"
+            element={<CalculatorProject />}
+          />
         </Routes>
       </BrowserRouter>
     </>
