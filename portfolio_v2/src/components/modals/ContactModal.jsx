0e01b4434033f6b96a68d8249cd544d5678bcf5d
dBish6 <davidbish2002@hotmail.com>
2023-01-11 21:02:59 -0330

    Carousel & Contact Modal Better Animations

--- portfolio_v2/src/components/modals/ContactModal.jsx
+++ portfolio_v2/src/components/modals/ContactModal.jsx
@@ -1,19 +1,18 @@
 import { useState, useEffect, useRef } from "react";
 
-import { motion, AnimatePresence } from "framer-motion";
-
 import { useForm } from "react-hook-form";
 import { ErrorMessage } from "@hookform/error-message";
 
 import emailjs from "@emailjs/browser";
 
 // *Design Imports*
+import { motion, AnimatePresence } from "framer-motion";
 import "./modals.css";
 
 // *Animations*
 const backdrop = {
-  visible: { opacity: 1 },
-  hidden: { opacity: 0 },
+  visible: { opacity: 1, zIndex: 10 },
+  hidden: { opacity: 0, zIndex: -99 },
 };
 
 const model = {
@@ -21,16 +20,20 @@ const model = {
     y: "-400%",
     x: "-50%",
     opacity: 0,
+    zIndex: -99,
+    // transition: {
+    //   y: { type: "ease" },
+    // },
   },
   visible: {
     y: "-50%",
     x: "-50%",
     opacity: 1,
+    zIndex: 10,
     // transition: { delay: 0.5 },
     transition: {
       // y: { type: "spring", stiffness: 180, damping: 30 },
       y: { type: "spring" },
-      opacity: { duration: 0.5 },
     },
   },
 };
@@ -59,7 +62,6 @@ const ContactModal = (props) => {
 
   const sendEmail = async () => {
     toggleLoading(true);
-    console.log(document.querySelector(".contactForm"));
     try {
       const res = await emailjs.sendForm(
         process.env.REACT_APP_EMAILJS_SERVICE_ID,
@@ -67,7 +69,7 @@ const ContactModal = (props) => {
         formRef.current,
         "-2fD04MRsvEcLZXA_"
       );
-      console.log(res);
+      // console.log(res);
 
       formRef.current.reset();
       toggleLoading(false);
@@ -78,140 +80,150 @@ const ContactModal = (props) => {
 
   return (
     <>
-      {props.show && (
-        <>
-          <AnimatePresence
-            mode="wait"
-            onExitComplete={() => props.setShow(false)}
+      {/* {props.show && ( */}
+      <>
+        <AnimatePresence
+          mode="wait"
+          // mode="sync"
+          // onExitComplete={() => props.setShow(false)}
+        >
+          <motion.div
+            variants={backdrop}
+            animate={props.show ? "visible" : "hidden"}
+            initial={props.show ? "hidden" : "visible"}
+            // FIXME:
+            exit={props.show ? "hidden" : "visible"}
+            transition={{ duration: 1 }}
+            key="backdrop"
+            className="darkBG"
+            onClick={() => props.setShow(false)}
+          />
+          <motion.div
+            variants={model}
+            animate={props.show ? "visible" : "hidden"}
+            initial="hidden"
+            exit="hidden"
+            key="model"
+            className="position"
           >
-            <motion.div
-              variants={backdrop}
-              animate="visible"
-              initial="hidden"
-              exit="hidden"
-              key="darkBG"
-              className="darkBG"
-              onClick={() => props.setShow(false)}
-            />
-            <motion.div
-              variants={model}
-              animate="visible"
-              initial="hidden"
-              exit="hidden"
-              key="position"
-              className="position"
-            >
-              <div className="contactModal">
-                <header className="modalHeader">
-                  <h3>Contact Me</h3>
-                </header>
-                <button
-                  className="closeBtn"
-                  onClick={() => props.setShow(false)}
-                >
-                  Close
-                </button>
+            <div className="contactModal">
+              <header className="modalHeader">
+                <h3>Contact Me</h3>
+              </header>
+              <button className="closeBtn" onClick={() => props.setShow(false)}>
+                Close
+              </button>
 
-                <motion.form
-                  variants={backdrop}
-                  animate={!loading ? "visible" : "hidden"}
-                  initial={!loading ? "hidden" : "visible"}
-                  exit={!loading ? "visible" : "hidden"}
-                  key={loading}
-                  className="contactForm"
-                  ref={formRef}
-                  onSubmit={handleSubmit(() =>
-                    sendEmail(watch("name"), watch("email"), watch("message"))
-                  )}
-                >
-                  <div>
-                    <input
-                      {...register("name", {
-                        required: "Name is required.",
-                        maxLength: {
-                          value: 30,
-                          message: "Name can be no more than 30 characters.",
-                        },
-                      })}
-                      name="name"
-                      id="nameInput"
-                      autoComplete="off"
-                      placeholder=" "
-                    />
-                    <label htmlFor="name" id="nameLabel">
-                      Name
-                    </label>
-                    <ErrorMessage
-                      errors={errors}
-                      name="name"
-                      render={({ message }) => (
-                        <small className="inputError" id="nameError">
-                          {message}
-                        </small>
-                      )}
-                    />
-
-                    <input
-                      {...register("email", {
-                        required: "Email is required.",
-                        pattern: {
-                          value:
-                            /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
-                          message: "Please enter a valid email.",
-                        },
-                      })}
-                      name="email"
-                      id="emailInput"
-                      autoComplete="off"
-                      placeholder=" "
-                    />
-                    <label htmlFor="email" id="emailLabel">
-                      Email
-                    </label>
-                    <ErrorMessage
-                      errors={errors}
-                      name="email"
-                      render={({ message }) => (
-                        <small className="inputError" id="emailError">
-                          {message}
-                        </small>
-                      )}
-                    />
-                  </div>
-
-                  <textarea
-                    {...register("message", {
-                      required: "Your message is required.",
+              <motion.form
+                // variants={backdrop}
+                animate={!loading ? { opacity: 1 } : { opacity: 0 }}
+                initial={!loading ? { opacity: 0 } : { opacity: 1 }}
+                exit={!loading ? { opacity: 1 } : { opacity: 0 }}
+                key={loading}
+                className="contactForm"
+                ref={formRef}
+                onSubmit={handleSubmit(() =>
+                  sendEmail(watch("name"), watch("email"), watch("message"))
+                )}
+              >
+                <div>
+                  <input
+                    {...register("name", {
+                      required: "Name is required.",
+                      maxLength: {
+                        value: 30,
+                        message: "Name can be no more than 30 characters.",
+                      },
                     })}
-                    name="message"
-                    id="messageTextarea"
-                    rows="6"
+                    name="name"
+                    id="nameInput"
                     autoComplete="off"
                     placeholder=" "
                   />
-                  <label htmlFor="message" id="messageLabel">
-                    Message
+                  <label htmlFor="name" id="nameLabel">
+                    Name
                   </label>
                   <ErrorMessage
                     errors={errors}
-                    name="message"
+                    name="name"
                     render={({ message }) => (
-                      <small className="inputError" id="messageError">
+                      <small className="inputError" id="nameError">
                         {message}
                       </small>
                     )}
                   />
 
-                  <button type="submit">
-                    <span className="btnTop">Send</span>
-                  </button>
-                </motion.form>
-                {loading ? <p>Loading...</p> : undefined}
-              </div>
-            </motion.div>
-          </AnimatePresence>
-        </>
-      )}
+                  <input
+                    {...register("email", {
+                      required: "Email is required.",
+                      pattern: {
+                        value: /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+                        message: "Please enter a valid email.",
+                      },
+                    })}
+                    name="email"
+                    id="emailInput"
+                    autoComplete="off"
+                    placeholder=" "
+                  />
+                  <label htmlFor="email" id="emailLabel">
+                    Email
+                  </label>
+                  <ErrorMessage
+                    errors={errors}
+                    name="email"
+                    render={({ message }) => (
+                      <small className="inputError" id="emailError">
+                        {message}
+                      </small>
+                    )}
+                  />
+                </div>
+
+                <textarea
+                  {...register("message", {
+                    required: "Your message is required.",
+                  })}
+                  name="message"
+                  id="messageTextarea"
+                  rows="6"
+                  autoComplete="off"
+                  placeholder=" "
+                  style={
+                    loading
+                      ? { visibility: "hidden" }
+                      : { visibility: "visible" }
+                  }
+                />
+                <label htmlFor="message" id="messageLabel">
+                  Message
+                </label>
+                <ErrorMessage
+                  errors={errors}
+                  name="message"
+                  render={({ message }) => (
+                    <small className="inputError" id="messageError">
+                      {message}
+                    </small>
+                  )}
+                />
+
+                <button type="submit">
+                  <span className="btnTop">Send</span>
+                </button>
+              </motion.form>
+              {loading ? (
+                <div className="contactLoader">
+                  <span />
+                  <span />
+                  <span />
+                </div>
+              ) : undefined}
+            </div>
+          </motion.div>
+        </AnimatePresence>
+      </>
+      {/* )} */}
     </>
   );
 };
