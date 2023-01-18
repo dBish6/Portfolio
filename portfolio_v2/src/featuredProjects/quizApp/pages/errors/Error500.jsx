2c2a0d35dea50a57d3a5dcb5b05eb5fb4404d0da
dBish6 <davidbish2002@hotmail.com>
2023-01-18 18:32:53 -0330

    Error Styles

--- portfolio_v2/src/featuredProjects/quizApp/pages/errors/Error500.jsx
+++ portfolio_v2/src/featuredProjects/quizApp/pages/errors/Error500.jsx
@@ -1,14 +1,17 @@
+import { useNavigate } from "react-router-dom";
 import useDocumentTitle from "../../../../hooks/useDocumentTitle";
-// import "./errors.css";
+import "./errors.css";
 
 const Error500 = (props) => {
   useDocumentTitle(`David Bishop | ${props.title}`);
+  const navigate = useNavigate();
 
   return (
-    <div className="errorContainer">
+    <div className="quizErrorContainer">
       <p>
         <span>Error 500:</span> API request failed.
       </p>
+      <button onClick={() => navigate(-1)}>Retry</button>
     </div>
   );
 };
