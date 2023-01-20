13066d29509361d50474cfad8a73a8547b666f18
dBish6 <davidbish2002@hotmail.com>
2023-01-20 17:13:51 -0330

    Date Addition for Project Dropdown

--- portfolio_v2/src/components/partials/Navigation.jsx
+++ portfolio_v2/src/components/partials/Navigation.jsx
@@ -17,6 +17,12 @@ const Navigation = () => {
   const [hamburgerDropdown, toggleHamburgerDropdown] = useState(false);
   const hamburgerDropdownRef = useRef(null);
 
+  const [showDatePopup, toggleDatePopup] = useState({
+    link1: false,
+    link2: false,
+    link3: false,
+  });
+
   // *Animations*
   const { scrollYProgress } = useScroll();
   const scaleX = useSpring(scrollYProgress, {
@@ -44,6 +50,39 @@ const Navigation = () => {
       },
       transitionEnd: { display: "none" },
     },
+  };
+
+  const popup = {
+    visible: {
+      y: "-4px",
+      opacity: 0.65,
+      fontSize: "14px",
+      display: "block",
+      transition: {
+        duration: 0.4,
+        type: "tween",
+      },
+    },
+    hidden: {
+      y: "9px",
+      opacity: 0,
+      fontSize: "10px",
+      transition: {
+        duration: 1,
+        type: "tween",
+      },
+      transitionEnd: { display: "none" },
+    },
+    // exit: {
+    //   y: "12px",
+    //   // opacity: 0.3,
+    //   fontSize: "0px",
+    //   transition: {
+    //     duration: 1,
+    //     type: "tween",
+    //   },
+    //   transitionEnd: { display: "none" },
+    // },
   };
 
   return (
@@ -79,21 +118,77 @@ const Navigation = () => {
                 <motion.ul
                   variants={fadeIn}
                   animate={projectsDropdown ? "visible" : "hidden"}
-                  initial={projectsDropdown ? "hidden" : "visible"}
-                  exit={projectsDropdown ? "hidden" : "visible"}
+                  initial="hidden"
+                  // exit={projectsDropdown ? "hidden" : "visible"}
                   className="options"
                 >
-                  <li>
+                  <motion.li
+                    onHoverStart={() => toggleDatePopup({ link1: true })}
+                    onHoverEnd={() => toggleDatePopup({ link1: false })}
+                  >
                     <NavLink to="/projects/todo">Todo List App</NavLink>
-                  </li>
+                  </motion.li>
+                  <div
+                    className={
+                      !showDatePopup.link1
+                        ? "datePopupContainer"
+                        : "datePopupContainer bg"
+                    }
+                    id="link1"
+                  >
+                    <motion.p
+                      variants={popup}
+                      animate={showDatePopup.link1 ? "visible" : "hidden"}
+                      initial="hidden"
+                      // exit="exit"
+                      className="datePopup"
+                    >
+                      April, 2022
+                    </motion.p>
+                  </div>
                   <hr />
-                  <li>
+                  <motion.li
+                    onHoverStart={() => toggleDatePopup({ link2: true })}
+                    onHoverEnd={() => toggleDatePopup({ link2: false })}
+                  >
                     <NavLink to="/projects/calculator">Calculator App</NavLink>
-                  </li>
+                  </motion.li>
+                  <div
+                    className={
+                      !showDatePopup.link2
+                        ? "datePopupContainer"
+                        : "datePopupContainer bg"
+                    }
+                    id="link2"
+                  >
+                    <motion.p
+                      variants={popup}
+                      animate={showDatePopup.link2 ? "visible" : "hidden"}
+                      initial="hidden"
+                      // exit="exit"
+                      className="datePopup"
+                    >
+                      October 29, 2022
+                    </motion.p>
+                  </div>
                   <hr />
-                  <li>
+                  <motion.li
+                    onHoverStart={() => toggleDatePopup({ link3: true })}
+                    onHoverEnd={() => toggleDatePopup({ link3: false })}
+                  >
                     <NavLink to="/projects/quiz">Quiz App</NavLink>
-                  </li>
+                  </motion.li>
+                  <div className={"datePopupContainer"}>
+                    <motion.p
+                      variants={popup}
+                      animate={showDatePopup.link3 ? "visible" : "hidden"}
+                      initial="hidden"
+                      // exit="exit"
+                      className="datePopup"
+                    >
+                      November, 2022
+                    </motion.p>
+                  </div>
                 </motion.ul>
               </AnimatePresence>
             </nav>
@@ -149,7 +244,7 @@ const Navigation = () => {
                 variants={fadeIn}
                 animate={hamburgerDropdown ? "visible" : "hidden"}
                 initial={hamburgerDropdown ? "hidden" : "visible"}
-                exit={hamburgerDropdown ? "hidden" : "visible"}
+                // exit={hamburgerDropdown ? "hidden" : "visible"}
               >
                 <div className="dropdownIcons">
                   <a
