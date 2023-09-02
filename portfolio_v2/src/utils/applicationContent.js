eca110ec734d9f7c8431562d0dd21803de7d9a41
David <davidbish2002@hotmail.com>
2023-09-01 23:37:48 -0230

    New application added and fixed overflowing image in carrousel.

--- portfolio_v2/src/utils/applicationContent.js
+++ portfolio_v2/src/utils/applicationContent.js
@@ -4,13 +4,23 @@ import ravenScreenshot from "../assets/images/Raven-Screenshot.png";
 import openWeatherScreenshot from "../assets/images/Open-Weather-Screenshot.png";
 import questCasinoScreenshot from "../assets/images/Quest-Casino-Screenshot.png";
 import scrollAnimationsDemo from "../assets/images/Scroll-Animations-Demo.png";
+import userAuthenticationDemos from "../assets/images/Ideal-User Authentication-Demos-Screenshot.png";
 
 export const content = [
+  {
+    id: 7,
+    title: "Ideal User Authentication Demos",
+    date: "June, 2023",
+    tech: "React, TypeScript, CSS, Node, Java, and Redis",
+    screenshot: userAuthenticationDemos,
+    alt: "Ideal User Authentication Demos Screenshot",
+    gitHub_URL: "https://github.com/dBish6/Ideal_User_Authentication_Demos",
+  },
   {
     id: 6,
     title: "Scroll Animations Demo",
     date: "June, 2023",
-    tech: "Vite, React, TypeScript",
+    tech: "Vite, React, TypeScript, and CSS",
     screenshot: scrollAnimationsDemo,
     alt: "Scroll Animations Demo Screenshot",
     gitHub_URL: "https://github.com/dBish6/Scroll_Animations_Examples",
@@ -20,7 +30,7 @@ export const content = [
     id: 5,
     title: "Quest Casino Full-Stack App",
     date: "Feb, 2023 - Current",
-    tech: "ReactJS, Chakra UI, NodeJS, Firebase",
+    tech: "ReactJS, Chakra UI, NodeJS, and Firebase",
     screenshot: questCasinoScreenshot,
     alt: "Quest Casino Screenshot",
     gitHub_URL: "https://github.com/dBish6/Quest_Casino_Full-Stack_App",
@@ -39,7 +49,7 @@ export const content = [
     id: 3,
     title: "Raven E-Commerce Full-Stack App",
     date: "Dec 2022",
-    tech: "ReactJS, CSS, Java, Docker and AWS.",
+    tech: "ReactJS, CSS, Java, SQL, Docker and AWS.",
     screenshot: ravenScreenshot,
     alt: "Raven Blacksmith Co Screenshot",
     gitHub_URL: "https://github.com/dBish6/Full-Stack_SpringAPI_Sprint2_Final",
@@ -50,7 +60,7 @@ export const content = [
     id: 2,
     title: "Relic Films Full-Stack App",
     date: "Oct 2022",
-    tech: "ReactJS, CSS, and NodeJS.",
+    tech: "ReactJS, CSS, NodeJS, and SQL.",
     screenshot: relicFilmsScreenshot,
     alt: "Relic Films Screenshot",
     gitHub_URL: "https://github.com/dBish6/Relic_Films_Full-Stack_App",
