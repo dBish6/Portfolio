7a4ed83df403efed648c8342a971e97f6e441d48
David <davidbish2002@hotmail.com>
2023-11-10 01:10:33 -0330

    Initial Layout

---
+++ portfolio_v3/src/pages/_app.tsx
@@ -0,0 +1,30 @@
+/* Portfolio Version 3
+ *
+ * @App Creation Date: November 7th, 2023
+ * @Last Updated: November 7th, 2023
+ *
+ * @Author David Bishop
+ *
+ * @Description .
+ */
+
+import type { AppProps } from "next/app";
+import dynamic from "next/dynamic";
+
+import { ToastContextProvider } from "@contexts/ToastContext";
+
+import Layout from "@components/partials/Layout";
+
+import "@styles/globals.css";
+
+export default function App({ Component, pageProps }: AppProps) {
+  // const DynamicComponent = dynamic(() => import('../components/'))
+
+  return (
+    <ToastContextProvider>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </ToastContextProvider>
+  );
+}
